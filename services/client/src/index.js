11ef4ed63c5254473e008dd69cb653e31c0a4573
John Kraus <john@johnfkraus.com>
2021-06-14 17:17:34 -0400

    work in process

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';  // new
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: []
+      users: [],
+      username: '', // new
+      email: '',    // new
     };
+    //this.addUser = this.addUser.bind(this);  // new
+    this.handleChange = this.handleChange.bind(this);
   }
 
   // new
   componentDidMount() {
     this.getUsers();
+  };
+
+  // addUser(event) {
+  //   event.preventDefault();
+  //   console.log('sanity check!');
+  // };
+
+  addUser = (event) => {
+    event.preventDefault();
+    console.log('sanity check!');
+    console.log(this.state);
+  };
+
+
+  handleChange(event) {
+    const obj = {};
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
   };
 
   // new
@@ -31,10 +54,19 @@ class App extends Component {
         <section className="section">
           <div className="container">
             <div className="columns">
-              <div className="column is-one-third">
+              <div className="column is-half">  {/* new */}
                 <br/>
                 <h1 className="title is-1">Users</h1>
                 <hr/><br/>
+                <AddUser
+                    username={this.state.username}
+                    email={this.state.email}
+                    addUser={this.addUser}
+                    // eslint-disable-next-line react/jsx-handler-names
+                    handleChange={this.handleChange}
+                />
+
+                <br/><br/>  {/* new */}
                 <UsersList users={this.state.users}/>
               </div>
             </div>
